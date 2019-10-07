c9ebf25dc14e988aba3af818c7096621abe85490
Anders Koch <andersclausenkoch@gmail.com>
2019-10-07 10:48:57 +0200

    first commit

---
+++ app/src/main/java/dk/anders/galgeleg1/MainActivity.java
@@ -0,0 +1,16 @@
+package dk.anders.galgeleg1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView visGalge;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        visGalge = findViewById(R.id.visGalge);
+    }
+}
