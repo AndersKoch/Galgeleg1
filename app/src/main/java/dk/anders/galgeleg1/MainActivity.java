edba80c9490c0b0e24a26557477943409fb88477
Anders Koch <andersclausenkoch@gmail.com>
2019-10-14 16:31:17 +0200

    Man kan nu gætte på ord og få respons.

--- app/src/main/java/dk/anders/galgeleg1/MainActivity.java
+++ app/src/main/java/dk/anders/galgeleg1/MainActivity.java
@@ -8,14 +8,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public ImageView visGalge;
     public Galgelogik spil = new Galgelogik();
     public EditText bogstav;
     public Button knap;
-    public TextView textSejre, textNederlag, textTitel;
-
+    public TextView textSejre, textNederlag, textTitel, ord, brugteBogstaver;
 
 
     @Override
@@ -31,14 +31,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textSejre = findViewById(R.id.textSejre);
         textNederlag = findViewById(R.id.textNederlag);
         textTitel = findViewById(R.id.textTitel);
-        
+        ord = findViewById(R.id.ordGæt);
 
+        ord.setText(spil.getSynligtOrd());
+    }
+
+    public void onClick(View v) {
+        if (v == knap) {
+            spil.gætBogstav(bogstav.getText().toString());
+            ord.setText(spil.getSynligtOrd());
+            String currentWord = "";
+
+            if (spil.erSidsteBogstavKorrekt() && !spil.erSpilletVundet()) {
+                Toast.makeText(this, "Rigtigt gæt! Du gættede på: " + bogstav.getText().toString().toUpperCase(), Toast.LENGTH_SHORT).show();
+            } else if (!spil.erSidsteBogstavKorrekt() && spil.getAntalForkerteBogstaver() < 6) {
+                Toast.makeText(this, "Ikke rigtigt. Dette var dit " + (spil.getAntalForkerteBogstaver()) + ". forkerte gæt", Toast.LENGTH_SHORT).show();
+            } else if (spil.erSpilletVundet()){
+                Toast.makeText(this, "FEDT MAN! DU GÆTTEDE ORDET! Ordet var: " + spil.getOrdet(), Toast.LENGTH_SHORT).show();
+            } else if (spil.erSpilletTabt()){
+                Toast.makeText(this, "Så tæt på, men du har tabt! Ordet var: " + spil.getOrdet(), Toast.LENGTH_SHORT).show();
+
+            }
+            // for (int i = 0; i < spil.getBrugteBogstaver().size(); i++){
+            //   currentWord = currentWord + " " + spil.getBrugteBogstaver().get(i);
+
+            //}
+            //brugteBogstaver.setText(currentWord);
+
+        }
+    }
+
+    public void billedeChanger() {
 
 
     }
-
-public void billedeChanger() {
-
-
-}
 }
